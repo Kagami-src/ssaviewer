2d5b944cca687c7ccd2be7652516839fb3310e42
kagami-src <xuxiaojie42@gmail.com>
2014-02-24 17:04:59 +0800

    up

--- SubPlayer/src/com/kagami/subplayer/MainActivity.java
+++ SubPlayer/src/com/kagami/subplayer/MainActivity.java
@@ -2,7 +2,10 @@ package com.kagami.subplayer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+				intent.setType("*/*");
+				intent.addCategory(Intent.CATEGORY_OPENABLE);
+				startActivityForResult(
+						Intent.createChooser(intent, "请选择一个要上传的文件"), 1);
+			}
+		});
 	}
 
 	@Override
